95df56439b6a41fdaf82e1a34accfefe3a1fe98d
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-14 22:13:55 +0100

    Full Description of my table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Printing the full description of full table.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
-FROM information_schema.COLUMNS
+SELECT TABLE_NAME, CREATE_TABLE_SQL
+FROM information_schema.TABLES
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';

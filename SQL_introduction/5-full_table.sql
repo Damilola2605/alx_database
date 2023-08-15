3e9304fa28d1c13b92a8d2da8b76a3ee8cd248f9
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-15 21:46:48 +0100

    Full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- Printing the full description of full table.
-SELECT create_statement
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+

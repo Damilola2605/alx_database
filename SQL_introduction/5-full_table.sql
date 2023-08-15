890a0050973cc113d0e58fc01a46178a732242cc
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-15 19:07:54 +0100

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
 -- Printing the full description of full table.
-SHOW FULL first_table
-    {FROM | IN} hbtn_0c_0;
+SELECT create_statement
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0'
+  AND table_name = 'first_table';

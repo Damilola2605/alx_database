bca67559940caaf3c4ab0a2d51d4449209a9e483
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-15 14:20:55 +0100

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Printing the full description of full table.
-SELECT TABLE_NAME, CREATE_TABLE_SQL
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE hbtn_0c_0.first_table;

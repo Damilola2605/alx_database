320d5f28915d63d8dbf11ab3ee8fbfb83dd95c03
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-15 20:14:52 +0100

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Creating a new table with multiple value
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
-);
+    );
 
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+INSERT INTO second_table (id, name, score) VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),

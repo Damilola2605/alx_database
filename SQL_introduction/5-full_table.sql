7de8d32eeb77dc1e1176d654bb8bd49e72dfbba3
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-15 18:45:17 +0100

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- Printing the full description of full table.
-SELECT create_statement
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table';
+SHOW FULL first_table
+    {FROM | IN} hbtn_0c_0;

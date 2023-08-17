df6816d9211970172609b2773f014887c6892f38
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-17 19:39:58 +0100

    Give it a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- Creating a table in my Mysql server
-CREATE TABLE IF NOT EXISTS force_name
+CREATE TABLE IF NOT EXISTS force_name(
     id INT,
-    name VARCHAR(256) NOT NULL;
+    name VARCHAR(256) NOT NULL
+);

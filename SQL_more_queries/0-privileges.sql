de8981200b8a4794e0fadcb8f53ee4c656f20156
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-17 13:09:58 +0100

    List privileges for users 1 & 2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- This script list priviledges of two users
-SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

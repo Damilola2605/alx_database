870fc53e375ca2fa15e5aeb384114f622ea0937b
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-17 19:32:44 +0100

    Making correction

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
 -- Creating a table in my Mysql server
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT
-    name VARCHAR(256) NOT NULL,
-);
+CREATE TABLE IF NOT EXISTS force_name
+    id INT,
+    name VARCHAR(256) NOT NULL;

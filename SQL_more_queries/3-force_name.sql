fcb2fd0fd328827021e7edc54224e37b0627954b
Damilola2605 <132350264+Damilola2605@users.noreply.github.com>
2023-08-17 14:56:27 +0100

    Give it a name to make more sense

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- Creating a table in my Mysql serverrrrrrrr
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
     );
